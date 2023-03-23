1990d0d82196dd94502d4cbeb2c9411755ea64e1
Admin <dinachakeeva@gmail.com>
2023-03-23 18:36:34 +0500

    KS-1-3 Gagarin task

---
+++ src/main/kotlin/lesson_1/lesson_1_task_3.kt
@@ -0,0 +1,13 @@
+package lesson_1
+
+fun main (){
+    val year:Int=1961
+    var hours:Int=09
+    var minutes:Int=07
+    println (year)
+    println (hours)
+    println (minutes)
+// Gagarin blasted off time
+    var hours= 10.toString()
+    var minutes=55
+    print (hours + ":" + minutes)
